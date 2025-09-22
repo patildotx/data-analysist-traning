cf9846c7da2769d4ff1b56a28c92d3652181ed12
patildotx <yashpratapsingh220@gmail.com>
2025-09-21 23:35:59 -0700

    Add files via upload

---
+++ aithmetic oparations.py
@@ -0,0 +1,17 @@
+a= int (input("enter 1st no"))
+b=int(input("Enter 2nd no."))
+c=(a+b)
+print ("division" , b/a)
+print('addition',c)
+print('mul', a*b )
+print("subs" , b-a)
+
+
+aa=input("enter ur name")
+print (aa)
+
+name=input("enter ur namer again")
+age= int(input("enter ur ummarrr"))
+city=input("enter ur city")
+
+print(name, age, city)
