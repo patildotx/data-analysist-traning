7ce1dfd8e012fee55110bc7c9c3a3f051a42008a
patildotx <yashpratapsingh220@gmail.com>
2025-09-23 01:55:31 -0400

    Add files via upload

---
+++ odd even.py
@@ -0,0 +1,22 @@
+'''a=int(input("enter a no."))
+
+if a%2 ==0:
+    print("odd")
+
+else : print("even")
+
+
+a=int(input("enter age"))'''
+
+'''for i in range (1,11):
+    i=i+i
+    
+    print(i)'''
+
+i=2
+while i<=100 :
+    
+    print(i)
+    i=i+2
+     
+
---
+++ table by python.py
@@ -0,0 +1,5 @@
+for i in range (1,11):
+    i=i+i
+    
+    print(i)
+
